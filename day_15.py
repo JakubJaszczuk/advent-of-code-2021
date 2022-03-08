72b739661dfe4c3a4bdebac52a4afaa73ce2ee05
JakubJaszczuk <238556@student.pwr.edu.pl>
2022-03-08 20:27:43 +0100

    Finished task 15

--- day_15.py
+++ day_15.py
@@ -6,60 +6,55 @@ def load_data(path):
         return np.array([[int(c) for c in line] for line in file.read().splitlines()])
 
 
-def get_adjacent(x, y, rows, cols):
-    adjacent = set()
+def get_adjacent(y, x, rows, cols):
+    adjacent = []
     if x-1 >= 0:
-        adjacent.add((y, x-1))
+        adjacent.append((y, x-1))
     if x+1 < cols:
-        adjacent.add((y, x+1))
+        adjacent.append((y, x+1))
     if y-1 >= 0:
-        adjacent.add((y-1, x))
+        adjacent.append((y-1, x))
     if y+1 < rows:
-        adjacent.add((y+1, x))
+        adjacent.append((y+1, x))
     return adjacent
 
 
-def generate_simple_path(matrix):
+def dijkstra(matrix):
     max_row = matrix.shape[0]
     max_col = matrix.shape[1]
-    path = [(0, v) for v in range(max_row)]
-    path.extend([(v, max_row-1) for v in range(1, max_col)])
-    return path
+    distances = np.full_like(matrix, np.iinfo(int).max)
+    prevs = np.full((max_row, max_col, 2), -1)
+    distances[0, 0] = 0
+    visited = set()
+    frontline = {(0, 0)}
+    while frontline:
+        coords = min(frontline, key=lambda x: distances[x])
+        frontline.remove(coords)
+        visited.add(coords)
+        if coords == (max_row-1, max_col-1):
+            return distances, prevs
+        adj = get_adjacent(*coords, max_row, max_col)
+        for n in (a for a in adj if a not in visited):
+            frontline.add(n)
+            dist = distances[coords] + matrix[n]
+            if dist < distances[n]:
+                distances[n] = dist
+                prevs[n] = coords
+    return distances, prevs
 
 
-def score(matrix, path):
-    score = -matrix[0, 0]
-    for p in path:
-        score += matrix[p]
-    return score
-
-
-def generate_paths(matrix):
-    max_row = matrix.shape[0]
-    max_col = matrix.shape[1]
-    best_path = generate_simple_path(matrix)  # Initial solution
-    best_score = score(matrix, best_path)
-    stack = [((0, 0), [])]  # Current location and path
-    while stack:
-        top, path = stack.pop()
-        q, w = top
-        top = w, q
-        path = path.copy()
-        path.append(top)
-        sc = score(matrix, path)
-        if sc > best_score:
-            continue
-        elif top == (max_row-1, max_col-1) and sc < best_score:
-            best_path = path
-            best_score = sc
-        else:
-            adj = get_adjacent(*top, max_row, max_col)
-            if len(path) > 1:
-                adj.remove(path[-2])
-            for e in adj:
-                stack.append((e, path))
-        #print(best_score, best_path)
-    return best_path
+def create_data_larger(matrix):
+    arr = matrix
+    for i in range(1, 5):
+        arr = np.append(arr, matrix + i, axis=1)
+        arr[arr > 9] += 1
+        arr = arr % 10
+    tmp = arr
+    for i in range(1, 5):
+        arr = np.append(arr, tmp + i, axis=0)
+        arr[arr > 9] += 1
+        arr = arr % 10
+    return arr
 
 
 def print_matrix(matrix):
@@ -68,14 +63,12 @@ def print_matrix(matrix):
 
 def main():
     data = load_data('day_15_data')
-    #print(data)
-    path = generate_paths(data)
-    print(score(data, path))
-    print(path)
-    vis = np.zeros(data.shape, dtype=bool)
-    for p in path:
-        vis[p] = True
-    print_matrix(vis)
+    distances, prevs = dijkstra(data)
+    print(distances[-1, -1])
+    # Task 2
+    larger_data = create_data_larger(data)
+    distances, prevs = dijkstra(larger_data)
+    print(distances[-1, -1])
 
 
 if __name__ == '__main__':
